d987f6ddde3af2d96c9790fafdd383695fb88706
cometyang <cometyang@gmail.com>
2021-01-15 22:41:41 -0600

    initial checkin servo_move_keyboard package

---
+++ src/servo_move_keyboard/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'servo_move_keyboard'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yang',
+    maintainer_email='cometyang@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
